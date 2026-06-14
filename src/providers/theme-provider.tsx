e9fdbb0ce39e34c5430c482874907f9cef2d0b96
Abhinav Kumar <iamabhinavkumar.21@gmail.com>
2026-06-14 11:08:48 +0530

    feat(theme): improve theme provider setup

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -1,15 +1,10 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes";
+import type { ComponentProps } from "react";
 
-export function ThemeProvider({
-  children,
-  ...props
-}: ThemeProviderProps) {
-  return (
-    <NextThemesProvider {...props}>
-      {children}
-    </NextThemesProvider>
-  );
+type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
