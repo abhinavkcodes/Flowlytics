fde4b3aefa9a5ef7a79f1f9d9613bf57dd53adaa
Abhinav Kumar <iamabhinavkumar.21@gmail.com>
2026-06-14 11:08:44 +0530

    chore(next): update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,14 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "github.com" },
+      { protocol: "https", hostname: "*.githubusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
